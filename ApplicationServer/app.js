cd79eaad100e0b066e60d134321625f131a36e21
Clemens Stift <clemens@stift.online>
2018-10-17 18:21:08 +0200

    Added tooling.

---
+++ ApplicationServer/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const debug = require('debug')('app');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, (req,res)=>{
+    res.send("Hi!");
+});
+
