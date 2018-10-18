20e4370005c26a122dfde9e0fa23b753192c75ca
Clemens Stift <clemens@stift.online>
2018-10-18 12:19:05 +0200

    Changed response to loading users and sending them as a JSON object for testing mongoDB.

--- ApplicationServer/app.js
+++ ApplicationServer/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const debug = require('debug')('app');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+app.use(morgan('short'));
 
-app.listen(port, (req,res)=>{
-    res.send("Hi!");
+const mongoOptions = {
+    user : "root",
+    pass : "example"
+}
+
+mongoose.connect(`mongodb://${mongoOptions.user}:${mongoOptions.pass}@mongo:27017`);
+
+const User = require("./models/User");
+
+app.use('/', (req,res)=>{
+    User.find({}).then((value)=>{
+        res.json(value);
+    });
 });
 
+app.listen(port);
+console.log(`Server started on port ${port}.`);
