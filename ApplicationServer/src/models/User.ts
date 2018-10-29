78108a5e82fc392989352827419a1cf9ff5137dd
Clemens Stift <clemens@stift.online>
2018-10-29 16:40:50 +0100

    Additional fields sketched for user - created during sprintmeeting with Norbert. Needs implementation.

--- ApplicationServer/src/models/User.ts
+++ ApplicationServer/src/models/User.ts
@@ -1,18 +1,14 @@
 import * as mongoose from 'mongoose';
 import * as findOrCreate from 'mongoose-findorcreate';
 
-class userSchemaClass extends mongoose.Schema {
-  constructor(definition? : mongoose.SchemaDefinition, options? : mongoose.SchemaOptions){
-    super(definition, options);
-  }
-}
-
 const userSchema : mongoose.Schema = new mongoose.Schema({
   email: {type: String, required: true},
   password: {type: String, required: true},
   username: {type: String, required: false, default: null},
   assigned: {type: Boolean, default: false},
-  address: {type: String, required:false, default: "0x0000000000000000000000000000000000000000"}
+  address: {type: String, required:false},
+  // passwordResetToken: {type: String, required: false},
+  // passwordResetTokenExpirationDate: {type: Date, required: false}
 });
 
 userSchema.methods.name = function() {
