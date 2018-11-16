c9fa38c53537e9eddc88319838cc903bf82aa432
Clemens Stift <clemens@stift.online>
2018-11-16 17:55:11 +0100

    Changed export style - same content.

--- ApplicationServer/models/User.js
+++ ApplicationServer/models/User.js
@@ -3,7 +3,7 @@ const findOrCreate = require('mongoose-findorcreate');
 
 const userSchema = mongoose.Schema({
   email: {type: String, required: true, unique: true},
-  username: {type: String, required: false},
+  username: {type: String, required: true},
   ethereumAddressAssigned: {type: Boolean, required: true, default: false},
   ethereumAddress: {type: String, required: false},
 });
