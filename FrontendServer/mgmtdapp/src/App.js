dd0af93a675b0c8f1852dba2ef4a0af813507290
Clemens Stift <clemens@stift.online>
2019-01-15 10:23:21 +0100

    Created Testlinks in react app.

--- FrontendServer/mgmtdapp/src/App.js
+++ FrontendServer/mgmtdapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TestLinkComponent from './components/TestLinkComponent';
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <TestLinkComponent></TestLinkComponent>
       </div>
     );
   }
---
+++ FrontendServer/mgmtdapp/src/components/TestLinkComponent.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class TestLinkComponent extends Component {
+  render() {
+    return (
+        <div>
+            <div>
+                <h3>
+                Login
+                </h3>
+                <a
+                className="App-link"
+                href="https://localhost:3001/account/login"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Login
+                </a>
+                <br></br>
+                <a
+                className="App-link"
+                href="https://localhost:3001/account/logout"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Logout
+                </a>
+            </div>
+            <div>
+                <h3>
+                User
+                </h3>
+                <a
+                className="App-link"
+                href="https://localhost:3001/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Get all data for user
+                </a>
+            </div>
+            <div>
+                <h3>
+                Organisations
+                </h3>
+                <a
+                className="App-link"
+                href="https://localhost:3001/organisation/create?organisationName=NextOrganisation"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                create Organisation
+                </a>
+            </div>
+            <div>
+                <h3>
+                Votes
+                </h3>
+                <a
+                className="App-link"
+                href="https://localhost:3001/vote/create?organisationId=5c3c82a15956a40020d8e79b&voteName=NewVote&descritpion=None&endTime=1550134736000"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                create Vote
+                </a>
+                <br></br>
+                <a
+                className="App-link"
+                href="https://localhost:3001/vote/cast?idOfVote=5c3c83175956a40020d8e7a3&organisationId=5c3c82a15956a40020d8e79b&vote=true"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                cast
+                </a>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default TestLinkComponent;
