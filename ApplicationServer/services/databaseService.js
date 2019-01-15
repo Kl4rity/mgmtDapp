2aba4065fe0c8de4b55effab9fef4d01a4b5c19b
Clemens Stift <clemens@stift.online>
2019-01-15 10:24:11 +0100

    Removed data logic from model - created service for CRUD operations as well as checking access rights.

---
+++ ApplicationServer/services/databaseService.js
@@ -0,0 +1,203 @@
+const mongoose = require('mongoose');
+const Organisation = mongoose.model('Organisation');
+const User = mongoose.model('User');
+const Vote = mongoose.model('Vote');
+
+const dataBaseService = {
+    organisation: {
+        create: (callingUser, nameOfOrganisation) => {
+            return (async function createOrganisation(){
+                let organisationReference = new Organisation();
+
+                organisationReference.name = nameOfOrganisation;
+                let newOrganisationMember = {
+                    userId: callingUser.id,
+                    role: 'admin'
+                }
+                organisationReference.members.push(newOrganisationMember);
+
+                console.log(organisationReference);
+                try {
+                    organisationReference.save();
+                    return true;
+                } catch {
+                    return false;
+                }
+            }())
+        }
+        , addMember: (organisationId, idOfMember) => {
+            return (async function executeAddMember() {
+                let organisationReference = await Organisation.findById(organisationId).exec();
+                organisationReference.members[idOfMember] = 'member';
+
+                try {
+                    organisationReference.save();
+                    return true;
+                } catch (err){
+                    console.log(err);
+                    return false;
+                }
+            }())
+        }
+        , removeMember: (organisationId, idOfMember) => {
+            return (async function executeRemoveMember() {
+                let organisationReference = await Organisation.findById(organisationId).exec();
+                delete organisationReference.members[idOfMember];
+
+                try {
+                    organisationReference.save();
+                    return true;
+                } catch (err){
+                    console.log(err);
+                    return false;
+                }
+            }())
+        }, close : (organisationId) => {
+            return (async function executeCloseOrganisation(){
+                let organisationReference = await Organisation.findById(organisationId).exec();
+                
+                try {
+                    organisationReference.remove();
+                    return true;
+                } catch (err){
+                    console.log(err);
+                    return false;
+                }
+            }())
+        }
+
+    }
+    , vote: {
+        create : (organisationId, nameOfVote, description, endDateUnix) => {
+            return (async function createVote(){
+                let voteReference = new Vote();
+
+                let targetOrganisation = await Organisation.findById(organisationId).exec();
+
+                voteReference.name = nameOfVote;
+                voteReference.description = description;
+                voteReference.endDate = endDateUnix;
+                let votes = [];
+
+                targetOrganisation.members.forEach((member)=>{
+                    votes.push({
+                        voter: member.userId
+                    });
+                });
+
+                voteReference.votes = votes;
+
+                try {
+                    voteReference.save().then((vote)=>{
+                        targetOrganisation.votes.push(vote._id);
+                        targetOrganisation.save();
+                        return true;
+                    });
+                } catch(err) {
+                    console.log(err)
+                    return false;
+                }
+            }())
+        },
+        close : (idOfVote) => {
+            return (async function closeVote(){
+                let voteReference = await Vote.findById(idOfVote).exec();
+
+                voteReference.ended = true;
+
+                try {
+                    voteReference.save();
+                    return true;
+                } catch(err) {
+                    console.log(err)
+                    return false;
+                }
+            }())
+        },
+        remove : (idOfVote) => {
+            return (async function removeVote(){
+                // Make these requests paralell with promises?
+                let voteReference = await Vote.findById(idOfVote).exec();
+
+                let organisationsWithVote = await Organisation.find({
+                    'votes' : [idOfVote]
+                }).exec();
+
+                let organisationReference = organisationsWithVote[0];
+
+                try {
+                    voteReference.remove().then(()=>{
+                        organisationReference.votes = organisationReference.votes.filter((vote)=>{
+                            return vote.id != idOfVote;
+                        });
+                        organisationReference.save();
+                    });
+                    return true;
+                } catch(err) {
+                    console.log(err)
+                    return false;
+                }
+            }())
+        },
+        cast : (idOfVote, userId, vote) => {
+            return (async function castVote(){
+                let voteReference = await Vote.findById(idOfVote).exec();
+                let userVoteIndex;
+                let userVote = voteReference.votes.filter((vote, index)=>{
+                    if(vote.voter == userId){
+                        userVoteIndex = index;
+                    }
+                    return vote.voter == userId;
+                });
+                // userVote.voter = mongoose.Types.ObjectId(userVote.voter);
+                userVote.vote = vote;
+                voteReference.votes[userVoteIndex] = userVote;
+                try {
+                    voteReference.save();
+                    return true;
+                } catch(err) {
+                    console.log(err)
+                    return false;
+                }
+            }())
+        },
+        changeEndDate : (idOfVote, newEndDate) => {
+            return (async function moveVoteEndDate(){
+                let voteReference = await Vote.findById(idOfVote).exec();
+                
+                if(Date.now() < newEndDate){
+                    voteReference.endDate = newEndDate;
+                } else {
+                    return false;
+                }
+
+                try {
+                    voteReference.save();
+                    return true;
+                } catch(err) {
+                    console.log(err)
+                    return false;
+                }
+            }())
+
+        }
+    }
+    , member : {
+        changeRole(idOfMember, newRole){
+            // Admin can do this.
+            return (async function changeMemberRole(){
+                let organisationReference = await Organisation.findById().exec();
+                organistationReference.members[idOfMember] = newRole;
+
+                try{
+                    organisationReference.save();
+                } catch (err) {
+                    console.log(err);
+                    return false;
+                }
+            }())
+        }
+    }
+}
+
+module.exports = dataBaseService;
---
+++ ApplicationServer/services/permissionService.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const Organisation = mongoose.model('Organisation');
+const userRoles = require('../models/Roles/roles');
+
+const permissionService = {
+    getRoleObjectForUserIdAndOrgansisation : (organisationId, callingUser) => {
+        return (async function checkUserPermission(){
+            let requestedOrganisation = await Organisation.findById(organisationId).exec();
+            let userMembership = requestedOrganisation.members.filter((member)=>{
+                return member.userId == callingUser.id;
+            })[0];
+
+            let roleObject = userRoles.filter((role)=>{
+                return role.name == userMembership.role;
+            })[0];
+
+            return roleObject;
+        }())
+    }
+    , field : {
+        vote : {
+            create : (organisationId, callingUser) => {
+                return (async function getCastCreatePermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.vote.create;
+                }());
+            },
+            close : (organisationId, callingUser) => {
+                return (async function getCastClosePermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.vote.close;
+                }());
+            },
+            remove : (organisationId, callingUser) => {
+                return (async function getCastRemovePermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.vote.remove;
+                }());
+            },
+            cast : (organisationId, callingUser) => {
+                return (async function getCastPermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.vote.cast;
+                }());
+            },
+            changeEndDate : (organisationId, callingUser) => {
+                return (async function getChangeEndDatePermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.vote.changeEndDate;
+                }());
+            }
+        }
+        , organisation : {
+            create : () => {
+                return true;
+            },
+            addMember : (organisationId, callingUser) => {
+                return (async function getAddMemberPermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.organisation.addMember;
+                }());
+            },
+            removeMember : (organisationId, callingUser) => {
+                return (async function getAddMemberPermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.organisation.removeMember;
+                }());
+            },
+            close : (organisationId, callingUser) => {
+                return (async function getAddMemberPermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.organisation.close;
+                }());
+            }
+        }
+        , member : {
+            changeRole : () => {
+                return (async function getAddMemberPermissionStatus(){
+                    let roleObject = await permissionService.getRoleObjectForUserIdAndOrgansisation(organisationId,callingUser);
+                    return roleObject.permissions.member.changeRole;
+                }());
+            }
+        }
+    }
+}
+
+module.exports = permissionService;
