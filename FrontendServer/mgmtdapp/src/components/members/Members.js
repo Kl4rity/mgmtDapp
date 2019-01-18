a1606f0f05049bfa60d0c77cd32eb532d26f4f7d
Clemens Stift <clemens@stift.online>
2019-01-18 11:29:31 +0100

    Logic for filtering out memberslist for selected organisation on link

--- FrontendServer/mgmtdapp/src/components/members/Members.js
+++ FrontendServer/mgmtdapp/src/components/members/Members.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
-export class Members extends Component {
+class Members extends Component {
+
+  constructor({match}){
+    super();
+    this.match = match;
+    this.displayMembersForId = this.displayMembersForId.bind(this);
+    this.filterMembers = this.filterMembers.bind(this);
+  }
+
+  componentWillReceiveProps({match}){
+    this.match = match;
+  }
+
+  displayMembersForId(id){
+    let members = this.filterMembers(id);
+    let memberList = null;
+    if(!!members && members.length > 0){
+      memberList = members.map((member, index)=>{
+        return <li key={index}>{member.id}</li>
+      });
+    } else {
+      return <li>No members to display.</li>
+    }
+    return memberList;
+  }
+
+  filterMembers(id){
+    let organisationMembers = null;
+    if(!!this.props.organisations & this.props.organisations.length > 0){
+      let currentOrganisation = this.props.organisations.filter(organisation => organisation.id == id)[0];
+      organisationMembers = currentOrganisation.members;
+    }
+    return organisationMembers;
+  }
+
   render() {
     return (
       <div>
-        This is a member list.
+        <ul>
+          {this.displayMembersForId(this.match.params.id)}
+        </ul>
       </div>
     )
   }
 }
 
-export default Members;
+function mapStateToProps(state, ownProps){
+  return {
+    organisations : state.organisations
+  }
+}
+
+export default connect(mapStateToProps)(Members);
