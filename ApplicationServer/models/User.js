9a21b21452f2eeed327a25ab758fded6d84badc3
Clemens Stift <clemens@stift.online>
2019-01-18 11:32:41 +0100

    Removed list of organisations from User model

--- ApplicationServer/models/User.js
+++ ApplicationServer/models/User.js
@@ -3,8 +3,7 @@ const findOrCreate = require('mongoose-findorcreate');
 
 const userSchema = mongoose.Schema({
   email: {type: String, required: true, unique: true},
-  username: {type: String, required: true},
-  organisations: {type: [mongoose.Schema.Types.ObjectId], required: false, ref: 'Organisation'}
+  username: {type: String, required: true}
 });
 
 userSchema.plugin(findOrCreate);
