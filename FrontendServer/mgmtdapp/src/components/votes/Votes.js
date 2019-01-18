8a4a4ed328c9616aa624cc71567403d6fde1b777
Clemens Stift <clemens@stift.online>
2019-01-18 11:30:00 +0100

    Logic for filtering out list of votes for selected organisation on link

--- FrontendServer/mgmtdapp/src/components/votes/Votes.js
+++ FrontendServer/mgmtdapp/src/components/votes/Votes.js
@@ -1,13 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export class Votes extends Component {
+class Votes extends Component {
+
+  constructor({match}){
+    super();
+    this.match = match;
+    this.displayVotesForId = this.displayVotesForId.bind(this);
+    this.filterVotes = this.filterVotes.bind(this);
+  }
+
+  componentWillReceiveProps({match}){
+    this.match = match;
+  }
+
+  displayVotesForId(id){
+    let votes = this.filterVotes(id);
+    let voteList = null;
+    if(votes.length > 0){
+      voteList = votes.map((vote, index)=>{
+         return <li key={index}>{vote.name}</li>
+      });
+    } else {
+      voteList = <li>No votes to display</li>
+    }
+    return voteList;
+  }
+
+  filterVotes(id){
+    let organisationVotes = null;
+    if(!!this.props.organisations & !!this.props.votes){
+      let currentOrganisation = this.props.organisations.filter(organisation => organisation.id == id)[0];
+      organisationVotes = this.props.votes.filter(vote => currentOrganisation.voteIds.indexOf(vote.id) >= 0);
+    }
+    return organisationVotes;
+  }
+
   render() {
     return (
       <div>
-        These are votes.
+        <ul>
+          {this.displayVotesForId(this.match.params.id)}
+        </ul>
       </div>
     )
   }
 }
 
-export default Votes;
+function mapStateToProps(state, ownProps){
+  return {
+    organisations : state.organisations,
+    votes : state.votes
+  }
+}
+
+export default connect(mapStateToProps)(Votes);
