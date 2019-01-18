23e95c344c679fcc46aebf376bdae5325ba233e2
Clemens Stift <clemens@stift.online>
2019-01-18 11:33:33 +0100

    Implemented permission-check in user-routes.

--- ApplicationServer/routes/user.routes.js
+++ ApplicationServer/routes/user.routes.js
@@ -5,30 +5,36 @@ const User = mongoose.model('User');
 const Organisation = mongoose.model('Organisation');
 const Vote = mongoose.model('Vote');
 const express = require('express');
+const insufficientPermissions = require('../util/insufficientPermissions');
 
 const userRouter = express.Router();
 
 const sendUserData = (req, res) => {
-    // Fetch all the data for a user.
-    (async function getOrganisationsForUser(){
-        let usersOrganisations = await Organisation.find({'members.userId': req.user.id}).exec();
-        let voteIds = [];
-        usersOrganisations.forEach((organisation)=>{
-            voteIds.push(...organisation.votes);
-        });
-        // Find votes for organisations instead - even if the user is not part of it.
-        userVotes = await Vote.find({
-            '_id' : {$in: voteIds.map(id => mongoose.Types.ObjectId(id))}
-        }).exec();
-
-        let responseObject = {
-            user : req.user,
-            usersOrganisations,
-            userVotes
-        }
-
-        res.send(responseObject);
-    }());
+    console.log(req.user);
+    if(!!req.user){
+        // Fetch all the data for a user.
+        (async function getOrganisationsForUser(){
+            let usersOrganisations = await Organisation.find({'members.userId': req.user.id}).exec();
+            let voteIds = [];
+            usersOrganisations.forEach((organisation)=>{
+                voteIds.push(...organisation.votes);
+            });
+            // Find votes for organisations instead - even if the user is not part of it.
+            userVotes = await Vote.find({
+                '_id' : {$in: voteIds.map(id => mongoose.Types.ObjectId(id))}
+            }).exec();
+    
+            let responseObject = {
+                user : req.user,
+                usersOrganisations,
+                userVotes
+            }
+    
+            res.send(responseObject);
+        }());
+    } else {
+        insufficientPermissions(req, res);
+    }
 }
 
 // If no User is logged in - send him to the login page.
