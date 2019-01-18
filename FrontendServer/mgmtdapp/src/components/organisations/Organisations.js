ef7e1de14c48d880d5f39e09e868d8445334d31b
Clemens Stift <clemens@stift.online>
2019-01-18 11:31:00 +0100

    Logic for displaying rudimentary list of organisations

--- FrontendServer/mgmtdapp/src/components/organisations/Organisations.js
+++ FrontendServer/mgmtdapp/src/components/organisations/Organisations.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
-export class Organisations extends Component {
+class Organisations extends Component {
+
+  organisationRow(organisation, index){
+    return <li key = {index}> <Link to={"/organisation/"+ organisation.id + "/votes/"}>{organisation.name} </Link></li>;
+  }
   render() {
+    let organisations;
+    if (this.props.organisations){
+      organisations = this.props.organisations.map(this.organisationRow);
+    } else {
+      organisations = <li>No organisations.</li>;
+    }
+
     return (
       <div>
-        This is the organisations-list.
+        <ul>
+          {organisations}
+        </ul>
       </div>
     )
   }
 }
 
-export default Organisations;
+function mapStateToProps(state, ownProps){
+    return {
+        organisations: state.organisations
+    }
+}
+
+Organisations.propTypes = {
+  organisations: PropTypes.array.isRequired
+}
+
+export default connect(mapStateToProps)(Organisations);
