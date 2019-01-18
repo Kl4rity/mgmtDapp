81c1a2f92baba4d2ba2b6273c53736c774c886f5
Clemens Stift <clemens@stift.online>
2019-01-18 11:27:47 +0100

    Implemented routing for inspecting an organisation's members and votes

--- FrontendServer/mgmtdapp/src/components/mainContainer/MainContainer.js
+++ FrontendServer/mgmtdapp/src/components/mainContainer/MainContainer.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import BlankHomePage from '../common/BlankHomePage';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Organisation from '../organisations/organisation/Organisation';
+import { Route, Switch } from 'react-router-dom';
+import Votes from '../votes/Votes'
 import Page404 from '../common/404';
 import Navbar from '../common/Navbar';
 import Organisations from '../organisations/Organisations';
+import {PropTypes} from 'prop-types';
 
-export class MainContainer extends Component {
+import {connect} from 'react-redux';
+
+import fetchDataServiceSingleton from '../../services/fetchDataService';
+import Members from '../members/Members';
+
+class MainContainer extends Component {
+
+  constructor(props, context){
+    super(props, context);
+    const fetchDataService = fetchDataServiceSingleton.getFetchDataService();
+    fetchDataService.fetchAllUserData();
+  }
   render() {
     return (
       <div>
         <Navbar/>
         <Organisations/>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/organisation" component={Organisation}/>
-            <Route path="/" exact component={BlankHomePage}/>
-            <Route component = {Page404}/>
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route path="/organisation/:id/votes/" component={Votes}/>
+          <Route path="/organisation/:id/members/" component={Members}/>
+          <Route path="/" exact component={BlankHomePage}/>
+          <Route component = {Page404}/>
+        </Switch>
       </div>
     )
   }
 }
 
-export default MainContainer;
+function mapStateToProps(state, ownProps){
+  return {
+    user: state.user
+  }
+}
+
+MainContainer.contextTypes = {
+  store: PropTypes.object
+};
+
+export default connect(mapStateToProps)(MainContainer);
