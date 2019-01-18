105b64c15e6bce74ab5663145987a214cc86d61a
Clemens Stift <clemens@stift.online>
2019-01-18 11:28:34 +0100

    Initialization of fetchUserDataService as well as Provider container for redux.

--- FrontendServer/mgmtdapp/src/App.js
+++ FrontendServer/mgmtdapp/src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainContainer from './components/mainContainer/MainContainer';
 
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
+import fetchDataServiceSingleton from './services/fetchDataService';
+
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.store = configureStore();
+    let dataServiceSingleton = fetchDataServiceSingleton.getFetchDataService(this.store);
+  }
   render() {
     return (
-      <div>
+      <Provider store = {this.store}>
         <BrowserRouter>
           <Switch>
             <Route path="/" component={MainContainer}/>
           </Switch>
         </BrowserRouter>
-      </div>
+      </Provider>
     );
   }
 }
