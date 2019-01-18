abeecb52d006bfdd6188d4efa86257d5748f9d3e
Clemens Stift <clemens@stift.online>
2019-01-18 11:29:06 +0100

    Implemented a Navbar for login and logout.

--- FrontendServer/mgmtdapp/src/components/common/Navbar.js
+++ FrontendServer/mgmtdapp/src/components/common/Navbar.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-          This is the navbar
-      </div>
+      <nav>
+          <a
+                className="App-link"
+                href="https://localhost:3001/account/login"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Login
+            </a>
+                <br></br>
+            <a
+                className="App-link"
+                href="https://localhost:3001/account/logout"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Logout
+            </a>
+      </nav>
     )
   }
 }
