9514e90c6ff593089acab268eaab87916f1fd8a7
Clemens Stift <clemens@stift.online>
2019-01-18 12:55:51 +0100

    Changed organisation schema to no longer reference userId but whole user object.

--- ApplicationServer/models/Organisation.js
+++ ApplicationServer/models/Organisation.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
-// const findOrCreate = require('mongoose-findorcreate');
 
 const organisationSchema = mongoose.Schema({
   name: {type: String, required: true},
   members: [{
-            userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+            user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
             role: {type: String}
         }],
-  votes: {type: [mongoose.Schema.Types.ObjectId], required: false, ref: 'Vote'}
+  votes: [{type: mongoose.Schema.Types.ObjectId, required: false, ref: 'Vote'}]
 });
-
-// organisationSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model("Organisation", organisationSchema);
