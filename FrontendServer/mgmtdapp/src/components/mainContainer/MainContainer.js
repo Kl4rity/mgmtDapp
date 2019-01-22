3dff64ce32d6b3208a56cf9c196b3a4e6b6aae7d
Clemens Stift <clemens@stift.online>
2019-01-22 17:11:10 +0100

    Implemented responsive navigation / content split

--- FrontendServer/mgmtdapp/src/components/mainContainer/MainContainer.js
+++ FrontendServer/mgmtdapp/src/components/mainContainer/MainContainer.js
@@ -4,18 +4,20 @@ import BlankHomePage from '../common/BlankHomePage';
 import { Route, Switch } from 'react-router-dom';
 import Votes from '../votes/Votes'
 import Page404 from '../common/404';
-import Navbar from '../common/Navbar';
 import Organisations from '../organisations/Organisations';
-import {PropTypes} from 'prop-types';
+import { PropTypes } from 'prop-types';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import fetchDataServiceSingleton from '../../services/fetchDataService';
 import Members from '../members/Members';
+import Organisation from '../organisations/organisation/Organisation';
+import { Container, Navbar, Row, Col } from 'react-materialize';
+import './MainContainer.css';
 
 class MainContainer extends Component {
 
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
     const fetchDataService = fetchDataServiceSingleton.getFetchDataService();
     fetchDataService.fetchAllUserData();
@@ -23,20 +25,31 @@ class MainContainer extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <Organisations/>
-        <Switch>
-          <Route path="/organisation/:id/votes/" component={Votes}/>
-          <Route path="/organisation/:id/members/" component={Members}/>
-          <Route path="/" exact component={BlankHomePage}/>
-          <Route component = {Page404}/>
-        </Switch>
+        <Organisations />
+        <Row className = 'mgmt-container'>
+          <Col className='mgmt-container' s={0} m={0} l={3}></Col>
+          <Col className='mgmt-container' s={12} m={12} l={9}>
+            <Navbar brand="MGMT" />
+          </Col>
+        </Row>
+          <Row>
+            <Col s={0} m={0} l={3}></Col>
+            <Col s={12} m={12} l={9}>
+              <Route path="/organisation/:id/" component={Organisation} />
+              <Switch>
+                <Route path="/organisation/:id/votes/" component={Votes} />
+                <Route path="/organisation/:id/members/" component={Members} />
+                <Route path="/" exact component={BlankHomePage} />
+                <Route component={Page404} />
+              </Switch>
+            </Col>
+          </Row>
       </div>
     )
   }
 }
 
-function mapStateToProps(state, ownProps){
+function mapStateToProps(state, ownProps) {
   return {
     user: state.user
   }
