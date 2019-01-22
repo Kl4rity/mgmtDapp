b08048252dccb1595feb43f0decde791048eb59e
Clemens Stift <clemens@stift.online>
2019-01-22 17:23:24 +0100

    Changed parsing logic for server response.

--- FrontendServer/mgmtdapp/src/services/fetchDataService.js
+++ FrontendServer/mgmtdapp/src/services/fetchDataService.js
@@ -1,10 +1,11 @@
-import {createOrganisations} from '../actions/organisationActions';
-import {readUserData} from '../actions/userActions';
+import { createOrganisations } from '../actions/organisationActions';
+import { readUserData } from '../actions/userActions';
+import { readRoleData } from '../actions/roleActions';
 
 const fetchDataServiceSingleton = {
-    serviceInstance : null,
-    getFetchDataService : function getFetchDataService(store){
-        if(!this.serviceInstance){
+    serviceInstance: null,
+    getFetchDataService: function getFetchDataService(store) {
+        if (!this.serviceInstance) {
             this.serviceInstance = new FetchDataService(store);
             return this.serviceInstance;
         } else {
@@ -16,40 +17,48 @@ const fetchDataServiceSingleton = {
 class FetchDataService {
     store = null;
 
-    constructor(store){
-        if(!store){
-            throw new Error("No store was passed to the constructor of the FetchDataService.");    
+    constructor(store) {
+        if (!store) {
+            throw new Error("No store was passed to the constructor of the FetchDataService.");
         }
         this.store = store;
     }
 
-    async fetchAllUserData(){
-        let response = await fetch('https://localhost:3001/',{
+    async fetchAllUserData() {
+        let response = await fetch('https://localhost:3001/', {
             credentials: 'include',
             headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-                },
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
             mode: 'cors'
-            })
-        return this.parseDataIntoReduxStore(this.convertJsonToDataStructure(await response.json()));
+        })
+        try {
+            let responseJSON = await response.json();
+            this.parseDataIntoReduxStore(this.convertJsonToDataStructure(responseJSON));
+        } catch (err) {
+            console.error("An error occured loading data from the backend.");
+            console.error(err);
+        }
     }
-    convertJsonToDataStructure(responseJSON){
+    convertJsonToDataStructure(responseJSON) {
         console.log(responseJSON);
         let appData = {};
         appData.user = {
-            username : responseJSON.user.username,
-            email : responseJSON.user.email,
-            id : responseJSON.user._id
+            username: responseJSON.user.username,
+            email: responseJSON.user.email,
+            id: responseJSON.user._id
         }
 
+        appData.roles = responseJSON.roles;
+
         appData.organisations = [];
-        if(responseJSON.organisations){
-            responseJSON.organisations.forEach((responseOrganisation)=>{
+        if (responseJSON.organisations) {
+            responseJSON.organisations.forEach((responseOrganisation) => {
                 let organisation = {};
 
                 organisation.members = [];
-                responseOrganisation.members.forEach((member)=>{
+                responseOrganisation.members.forEach((member) => {
                     let newMemberObject = {};
                     newMemberObject.role = member.role;
                     newMemberObject.id = member.user._id;
@@ -59,7 +68,7 @@ class FetchDataService {
                 });
 
                 organisation.votes = [];
-                responseOrganisation.votes.forEach((vote)=>{
+                responseOrganisation.votes.forEach((vote) => {
                     let newVoteObject = {};
                     newVoteObject.creationDate = vote.created;
                     newVoteObject.endDate = vote.endDate;
@@ -68,10 +77,11 @@ class FetchDataService {
                     let newVoteDescription = (vote.description) ? vote.desccription : null;
                     newVoteObject.description = newVoteDescription;
                     newVoteObject.votes = [];
-                    newVoteObject.votes.forEach((individualVote)=>{
+                    vote.votes.forEach((individualVote) => {
                         let newIndividualVote = {};
                         newIndividualVote.response = individualVote.vote;
-                        newIndividualVote.voter = responseOrganisation.members.filter((member)=>{return member.id == individualVote.voter})[0];
+                        newIndividualVote.voter = responseOrganisation.members.filter((member) => { return member.id == individualVote.voter })[0];
+                        newVoteObject.votes.push(newIndividualVote);
                     });
                     organisation.votes.push(newVoteObject);
                 });
@@ -83,9 +93,12 @@ class FetchDataService {
         }
         return appData;
     }
-    parseDataIntoReduxStore(appData){
+    parseDataIntoReduxStore(appData) {
         this.store.dispatch(createOrganisations(appData.organisations));
-        this.store.dispatch(readUserData(appData.user));
+        if (!!this.store.getState().user) {
+            this.store.dispatch(readUserData(appData.user));
+            this.store.dispatch(readRoleData(appData.roles));
+        }
     }
 }
 
