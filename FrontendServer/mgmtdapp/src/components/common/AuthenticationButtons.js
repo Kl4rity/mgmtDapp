bed53fad03e47e57f42169e61027ca4510986d6c
Clemens Stift <clemens@stift.online>
2019-01-22 17:19:12 +0100

    Implemented login button - now depends on whether user is logged in or out.

---
+++ FrontendServer/mgmtdapp/src/components/common/AuthenticationButtons.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {SideNavItem} from 'react-materialize';
+import {Link} from 'react-router-dom';
+
+class AuthenticationButtons extends Component {
+    
+    constructor(props){
+        super();
+        this.displayLoginLogout = this.displayLoginLogout.bind(this);
+    }
+    displayLoginLogout() {
+        let button = null;
+        console.log(this.props.user.id);
+        if (!this.props.user.id) {
+            button = <div><SideNavItem subheader>Account</SideNavItem> <SideNavItem href="https://localhost:3001/account/login">Login</SideNavItem></div>
+        } else {
+            button = <div><SideNavItem subheader>Account</SideNavItem> <SideNavItem href="https://localhost:3001/account/logout">Logout</SideNavItem></div>
+        }
+        return button;
+    }
+  
+    render() {
+    return this.displayLoginLogout()
+  }
+}
+
+function mapStateToProps (state, ownProps) {
+  return {
+      user : state.user
+  }
+}
+
+export default connect(mapStateToProps)(AuthenticationButtons)
