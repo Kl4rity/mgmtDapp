bc07f9d2efd467f2ff32d0176166aa38d7dd0044
Clemens Stift <clemens@stift.online>
2019-01-23 13:09:33 +0100

    Added deleteOrganisation dialogue and display-logic.

---
+++ FrontendServer/mgmtdapp/src/components/organisations/DeleteOrganisation/DeleteOrganisation.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import ApprovalDialogue from '../../common/ApprovalDialogue';
+import roleUtils from '../../utils/roleUtils';
+import postDataServiceSingleton from '../../../services/postService';
+
+export class DeleteOrganisation extends Component {
+
+    constructor(props){
+        super(props);
+        this.deleteOrganisation = this.deleteOrganisation.bind(this);
+        this.memberCanDeleteOrganisation = this.memberCanDeleteOrganisation.bind(this);
+        this.displayDeleteOption = this.displayDeleteOption.bind(this);
+        this.postDataService = postDataServiceSingleton.getPostServiceInstance();
+        if(props.organisationId){
+            console.log("An organisationID");
+            console.log(props.organisationId);
+        }
+    }
+
+    deleteOrganisation(organisationId){
+        console.log("This is the ID being sent:");
+        console.log(organisationId);
+        this.postDataService.postData('https://localhost:3001/organisation/close/', {
+            organisationId : organisationId
+        });
+    }
+
+    memberCanDeleteOrganisation(user, memberList, rolesList) {
+        let canRemoveOrganisation = false;
+        let userRole = roleUtils.getRoleForUserInOrganisation(user, memberList, rolesList);
+        if (!!userRole) {
+            canRemoveOrganisation = userRole.permissions.organisation.close;
+        }
+        return canRemoveOrganisation;
+    }
+
+    displayDeleteOption(){
+        let option = null;
+        if(this.memberCanDeleteOrganisation(this.props.user, this.props.memberList, this.props.roles)){
+            option = <ApprovalDialogue args={[this.props.organisationId]} onApproval={this.deleteOrganisation} trigger = {<span className = "mgmt-organisations-navitem-align-right"> X </span>}></ApprovalDialogue>
+        }
+        return option;
+    }
+
+  render() {
+    return (
+      this.displayDeleteOption()
+    )
+  }
+}
+
+export default DeleteOrganisation;
