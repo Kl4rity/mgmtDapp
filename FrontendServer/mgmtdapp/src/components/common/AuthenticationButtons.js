0343afa5248cc3e9be0ee94878a2e0bb3b1d934f
Clemens Stift <clemens@stift.online>
2019-01-23 13:49:27 +0100

    Removed reference to unused.

--- FrontendServer/mgmtdapp/src/components/common/AuthenticationButtons.js
+++ FrontendServer/mgmtdapp/src/components/common/AuthenticationButtons.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {SideNavItem} from 'react-materialize';
-import {Link} from 'react-router-dom';
-
 class AuthenticationButtons extends Component {
     
     constructor(props){
